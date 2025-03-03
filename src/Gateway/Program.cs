7a73db99b842f146c5cf2f3c77c72a9baf53eaa9
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2025-03-03 14:02:06 -0300

    Some fixes

--- src/Gateway/Program.cs
+++ src/Gateway/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOcelot();
 builder.Configuration.AddJsonFile("ocelot.json");
+builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
 
 var app = builder.Build();
 await app.UseOcelot();
