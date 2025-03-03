a5d97e96a3e4c4fe0a788821aa8c73215673d6a3
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2025-03-03 12:14:38 -0300

    Created tests for CreateSaleCommandHandler

---
+++ src/Sales.Tests/Application/Handlers/Sales/CreateSaleCommandHandlerTests.cs
@@ -0,0 +1,120 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using FluentValidation;
+using Moq;
+using Sales.Application.Commands.Sales;
+using Sales.Application.DTOs;
+using Sales.Application.Handlers.Sales;
+using Sales.Application.Interfaces.Repositories;
+using Sales.Application.Shared;
+using Sales.Application.Shared.Enum;
+using Sales.Application.Validators.Sales;
+using Sales.Domain.Entities;
+using Sales.Tests.Builders.Commands;
+using Sales.Tests.Builders.DTOs;
+using Sales.Tests.Builders.Entities;
+using Xunit;
+
+namespace Sales.Tests.Application.Handlers.Sales
+{
+    public class CreateSaleCommandHandlerTests
+    {
+        private readonly Mock<IProductRepository> _productRepositoryMock;
+        private readonly Mock<ISaleRepository> _saleRepositoryMock;
+        private readonly Mock<IMapper> _mapperMock;
+        private readonly CreateSaleCommandValidator _validator;
+        private readonly Mock<IValidator<SaleItemCommand>> _commandValidator;
+        private readonly CreateSaleCommandHandler _handler;
+
+        public CreateSaleCommandHandlerTests()
+        {
+            _productRepositoryMock = new Mock<IProductRepository>();
+            _saleRepositoryMock = new Mock<ISaleRepository>();
+            _mapperMock = new Mock<IMapper>();
+            _commandValidator = new Mock<IValidator<SaleItemCommand>>();
+            _validator = new CreateSaleCommandValidator(_commandValidator.Object);
+            _handler = new CreateSaleCommandHandler(_productRepositoryMock.Object, _saleRepositoryMock.Object, _mapperMock.Object, _validator);
+        }
+
+        [Fact]
+        public async Task Handle_ShouldCreateSale_WhenRequestIsValid()
+        {
+            // Arrange
+            var saleItems = new List<SaleItemCommand>() {
+                new SaleItemCommandBuilder().Build(),
+                new SaleItemCommandBuilder().Build()
+            };
+
+            var command = new CreateSaleCommandBuilder()
+                .WithItems(saleItems)
+                .Build();
+
+            var saleDto = new SaleDtoBuilder().Build();
+
+            _productRepositoryMock.Setup(r => r.GetByIdsAsync(It.IsAny<HashSet<Guid>>()))
+                .ReturnsAsync(command.Items.Select(it => new ProductBuilder().WithId(it.ProductId).Build()));
+
+            _mapperMock.Setup(m => m.Map<Sale>(It.IsAny<CreateSaleCommand>()))
+                .Returns(new Sale());
+
+            _saleRepositoryMock.Setup(r => r.AddAsync(It.IsAny<Sale>()))
+                .ReturnsAsync(new Sale());
+
+            _mapperMock.Setup(m => m.Map<SaleDto>(It.IsAny<Sale>()))
+                .Returns(saleDto);
+
+            // Act
+            var result = await _handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            result.Data.Should().BeEquivalentTo(saleDto);
+            result.Status.Should().Be(ResultResponseKind.DataPersisted);
+            result.Message.Should().Be(string.Format(Consts.EntityCreatedWithSuccess, nameof(Sale)));
+        }
+
+        [Fact]
+        public async Task Handle_ShouldReturnNotFound_WhenSomeProductsAreMissing()
+        {
+            // Arrange
+            var saleItems = new List<SaleItemCommand>() {
+                new SaleItemCommandBuilder().Build(),
+                new SaleItemCommandBuilder().Build()
+            };
+
+            var command = new CreateSaleCommandBuilder()
+                .WithItems(saleItems)
+                .Build();
+
+            var existingProductIds = command.Items.Take(1).Select(it => it.ProductId);
+
+            _productRepositoryMock.Setup(r => r.GetByIdsAsync(It.IsAny<HashSet<Guid>>()))
+                .ReturnsAsync(existingProductIds.Select(id => new ProductBuilder().WithId(id).Build()));
+
+            // Act
+            var result = await _handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            result.Data.Should().BeNull();
+            result.Status.Should().Be(ResultResponseKind.NotFound);
+            result.ErrorType.Should().Be(ErrorType.DataNotFound);
+            result.ErrorDetail.Should().Contain(string.Join(", ", command.Items.Select(it => it.ProductId).Except(existingProductIds)));
+        }
+
+        [Fact]
+        public async Task Handle_ShouldThrowValidationException_WhenValidationFails()
+        {
+            // Arrange
+            var command = new CreateSaleCommandBuilder().
+                WithSaleDate(default)
+                .Build();
+
+            // Act
+            Func<Task> act = () => _handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            await act.Should().ThrowAsync<ValidationException>();
+            _productRepositoryMock.Verify(r => r.GetByIdsAsync(It.IsAny<HashSet<Guid>>()), Times.Never);
+            _saleRepositoryMock.Verify(r => r.AddAsync(It.IsAny<Sale>()), Times.Never);
+        }
+    }
+}
---
+++ src/Sales.Tests/Builders/Entities/ProductBuilder.cs
@@ -0,0 +1,68 @@
+﻿using Bogus;
+using Sales.Domain.Entities;
+using System.Reflection;
+
+namespace Sales.Tests.Builders.Entities
+{
+    public class ProductBuilder
+    {
+        private readonly Faker _faker = new();
+        private readonly Product _instance;
+
+        public ProductBuilder()
+        {
+            _instance = (Product)Activator.CreateInstance(typeof(Product), nonPublic: true)!;
+
+            WithId(Guid.NewGuid())
+            .WithTitle(_faker.Commerce.ProductName())
+            .WithDescription(_faker.Commerce.ProductDescription())
+            .WithPrice(_faker.Finance.Amount())
+            .WithCategory(_faker.Commerce.Categories(1).First())
+            .WithImage(_faker.Image.PicsumUrl());
+        }
+
+        public ProductBuilder WithId(Guid id)
+        {
+            SetPrivateField(nameof(Product.Id), id);
+            return this;
+        }
+
+        public ProductBuilder WithTitle(string title)
+        {
+            SetPrivateField(nameof(Product.Title), title);
+            return this;
+        }
+
+        public ProductBuilder WithDescription(string description)
+        {
+            SetPrivateField(nameof(Product.Description), description);
+            return this;
+        }
+
+        public ProductBuilder WithPrice(decimal price)
+        {
+            SetPrivateField(nameof(Product.Price), price);
+            return this;
+        }
+
+        public ProductBuilder WithCategory(string category)
+        {
+            SetPrivateField(nameof(Product.Category), category);
+            return this;
+        }
+
+        public ProductBuilder WithImage(string image)
+        {
+            SetPrivateField(nameof(Product.Image), image);
+            return this;
+        }
+
+        public Product Build() => _instance;
+
+        private void SetPrivateField(string fieldName, object value)
+        {
+            var field = _instance.GetType().GetField($"<{fieldName}>k__BackingField", BindingFlags.Instance | BindingFlags.NonPublic);
+            field?.SetValue(_instance, value);
+        }
+    }
+}
